ddebe20d0b3c5e6a642fcad4883dd4bb7a7adb91
saikrishna <Saikrishna.GV@excelenciaconsulting.com>
2022-10-27 01:53:56 +0530

    Asset type cmt -1 sai FE

--- src/app/Assets/asset-type/asset-type.component.ts
+++ src/app/Assets/asset-type/asset-type.component.ts
@@ -23,16 +23,20 @@ export class AssetTypeComponent implements OnInit {
   assetTypeForm!: FormGroup;
 
   // public AssetTypeList:type[] = [];
-    
+  AssetTypeList:any;
+  AssetTypeListTemp:any;
+  AssetTypeEdit:any;
+  AssetTypeEditTemp:any;
   constructor(public formBuilder: FormBuilder, public AssetTypeService:AssetTypeService) { }
 
   ngOnInit(): void {
 
     this.assetTypeForm = this.formBuilder.group({
+      Id:[''],
       AssetTypeName : ['',Validators.required]
    })
 
-  //  this.getAssetType();
+    this.getAssetType();
 
   }
   
@@ -49,23 +53,69 @@ export class AssetTypeComponent implements OnInit {
 
 saveAsset(){
   this.submitted=true;
-  console.log("hi")
+  console.log("this.assetTypeForm.value",this.assetTypeForm.value);
+  
+  if(this.assetTypeForm.value.Id == undefined){
+    this.assetTypeForm.get("Id")?.setValue(0);
+  }
+  if(this.assetTypeForm.value.Id == 0){
+  console.log("this.assetTypeForm.value2222",this.assetTypeForm.value);
   if(this.assetTypeForm.valid){
-    console.log("this.assetTypeForm",this.assetTypeForm.value);
     this.AssetTypeService.AssetTypePost(this.assetTypeForm.value).subscribe(res=>{  
     });
 
+  }      
+ }
+  else{
+  if(this.assetTypeForm.valid){
+  this.AssetTypeService.EditAssetType(this.assetTypeForm.value).subscribe((res)=>{ 
+    this.AssetTypeListTemp = res;
+    console.log("this.AssetTypeList",this.AssetTypeListTemp);
+  });
+ }
   }
 }
 
 
-// getAssetType(){
-//   this.AssetTypeService.GetAssetType().subscribe(res=>{ 
-//     console.log("result",res); 
-//     this.AssetTypeList = res;
-//     console.log("this.AssetTypeList",this.AssetTypeList);
-//   });
-//  // console.log("this.AssetTypeList",this.AssetTypeList);
-// }
+getAssetType(){
+  this.AssetTypeService.GetAssetType().subscribe((res)=>{ 
+    this.AssetTypeListTemp = res;
+    console.log("this.AssetTypeList",this.AssetTypeListTemp);
+  });
+  setTimeout (() => {
+    this.AssetTypeList =this.AssetTypeListTemp;
+  console.log("this.AssetTypeList2",this.AssetTypeList);},1000)
+}
+
+EditAssetType(id : any,){
+  this.assetDialog = true;
+  console.log("dadad",id);
+  this.AssetTypeService.GetEditAssetType(id).subscribe((res)=>{ 
+    console.log("res",res);
+    this.AssetTypeEditTemp = res;
+  });
+  setTimeout (() => {
+    this.AssetTypeEdit =this.AssetTypeEditTemp;
+  console.log("this.AssetTypeEditTemp",this.AssetTypeEdit)
+  this.assetTypeForm.get("Id")?.patchValue(this.AssetTypeEdit.id);
+  this.assetTypeForm.get("AssetTypeName")?.patchValue(this.AssetTypeEdit.assetTypeName);},500)
+
+  
+
+
+
+}
+
+DelteAssetType(id : number){
+console.log("Deleteid",id);
+if(id!=0){
+    this.AssetTypeService.DeleteAssetType(id).subscribe((res)=>{ 
+     console.log("this.AssetTypeList",this.AssetTypeListTemp);
+      });
+}
+
+}
+
+
 
 }
--- src/app/Services/asset-type.service.ts
+++ src/app/Services/asset-type.service.ts
@@ -21,14 +21,38 @@ export class AssetTypeService {
   }
     
   
-  // GetAssetType()
-  //   {
-  //     return this.httpClient.get<any>(this.Apiurl+'/AssetType/GetAssetType',{
-  //       headers:new HttpHeaders({
-  //         "content-type":"application/json"
-  //       })
-  //     })
-  //   }
+  GetAssetType()
+    {
+      return this.httpClient.get<any>(this.Apiurl+'/AssetType/GetAssetType',{
+        headers:new HttpHeaders({
+          "content-type":"application/json"
+        })
+      })
+    }
+
+    EditAssetType(EditAssetFromDate : any){
+      return this.httpClient.put<any>(this.Apiurl+'/AssetType/EditAssetTypeForm',EditAssetFromDate,{
+        headers:new HttpHeaders({
+          "content-type":"application/json"
+        })
+      })
+    }
+
+    DeleteAssetType(id: any){
+      return this.httpClient.delete<any>(this.Apiurl+'/AssetType/DeleteAssetType/'+id,{
+        headers:new HttpHeaders({
+          "content-type":"application/json"
+        })
+      })
+    }
+
+    GetEditAssetType(id: number){
+      return this.httpClient.get<any>(this.Apiurl+'/AssetType/GetEditAssetType/'+id,{
+        headers:new HttpHeaders({
+          "content-type":"application/json"
+        })
+      })
+    }
   
 }
 

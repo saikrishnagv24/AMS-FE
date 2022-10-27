af0f2220ca49e5326cc79f7cfdd5aa17afa73a66
saikrishna <Saikrishna.GV@excelenciaconsulting.com>
2022-10-27 22:25:14 +0530

    Asset type All completed except search and export.

--- src/app/Assets/asset-type/asset-type.component.ts
+++ src/app/Assets/asset-type/asset-type.component.ts
@@ -1,6 +1,7 @@
 import { IfStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { AssetTypeService } from 'src/app/Services/asset-type.service';
 
 // export interface type{
@@ -22,12 +23,18 @@ export class AssetTypeComponent implements OnInit {
 
   assetTypeForm!: FormGroup;
 
+  displayDeleteConfirmation: boolean = false;
+
+  DelteId!:number;
+
+  DeleteAssetType:any;
+
   // public AssetTypeList:type[] = [];
   AssetTypeList:any;
   AssetTypeListTemp:any;
   AssetTypeEdit:any;
   AssetTypeEditTemp:any;
-  constructor(public formBuilder: FormBuilder, public AssetTypeService:AssetTypeService) { }
+  constructor(public formBuilder: FormBuilder, public AssetTypeService:AssetTypeService,private messageService: MessageService) { }
 
   ngOnInit(): void {
 
@@ -49,31 +56,40 @@ export class AssetTypeComponent implements OnInit {
   hideDialog() {
     this.assetDialog = false;
     this.submitted = false;
-}
+    this.assetTypeForm.reset();
+  }
 
 saveAsset(){
   this.submitted=true;
   console.log("this.assetTypeForm.value",this.assetTypeForm.value);
   
-  if(this.assetTypeForm.value.Id == undefined){
+  if(this.assetTypeForm.value.Id == undefined || this.assetTypeForm.value.Id==''){
     this.assetTypeForm.get("Id")?.setValue(0);
   }
   if(this.assetTypeForm.value.Id == 0){
   console.log("this.assetTypeForm.value2222",this.assetTypeForm.value);
   if(this.assetTypeForm.valid){
-    this.AssetTypeService.AssetTypePost(this.assetTypeForm.value).subscribe(res=>{  
+    this.AssetTypeService.AssetTypePost(this.assetTypeForm.value).subscribe(res=>{ 
+      this.getAssetType(); 
+      this.messageService.add({severity:'success', summary: 'Success', detail: 'Asset type added'});
     });
 
-  }      
- }
+  }
+  this.assetDialog = false;    
+  }
   else{
   if(this.assetTypeForm.valid){
   this.AssetTypeService.EditAssetType(this.assetTypeForm.value).subscribe((res)=>{ 
     this.AssetTypeListTemp = res;
+    this.getAssetType();
     console.log("this.AssetTypeList",this.AssetTypeListTemp);
+    this.messageService.add({severity:'success', summary: 'Success', detail: 'Asset type added'});
   });
- }
+  this.assetDialog = false; 
   }
+  }
+  this.assetTypeForm.reset();
+
 }
 
 
@@ -101,16 +117,34 @@ EditAssetType(id : any,){
   this.assetTypeForm.get("AssetTypeName")?.patchValue(this.AssetTypeEdit.assetTypeName);},500)
 }
 
-DelteAssetType(id : number){
-console.log("Deleteid",id);
-if(id!=0){
-    this.AssetTypeService.DeleteAssetType(id).subscribe((res)=>{ 
-     console.log("this.AssetTypeList",this.AssetTypeListTemp);
-      });
-}
+DelteAssetType(id : number,assetTypeName : any){
+  this.displayDeleteConfirmation=true;
+  console.log("Deleteid",id);
+  this.DelteId=0;
+  this.DeleteAssetType = null;
+  this.DelteId=id;
+  this.DeleteAssetType = assetTypeName;
+  }
 
-}
+  yesDelete(){
+    console.log(this.DelteId);
+    console.log(this.DeleteAssetType)
 
+    if(this.DelteId!=0){
+      this.AssetTypeService.DeleteAssetType(this.DelteId).subscribe((res)=>{ 
+       console.log("this.AssetTypeList",this.AssetTypeListTemp);
+       this.getAssetType();
+       this.messageService.add({severity:'success', summary: 'Success', detail: 'Asset type '+ this.DeleteAssetType +' Deleted'});
+        });
+      }
+      this.displayDeleteConfirmation = false;
+      setTimeout (() => {
+        this.DelteId=0;
+        this.DeleteAssetType=null},500);
+  }
 
-
+  noDelete(){
+    console.log(this.DelteId);
+    this.displayDeleteConfirmation = false;
+  }
 }
